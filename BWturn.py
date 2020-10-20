34fbe89a5e5f301364976aa226579aaad4babc92
Kengo Horii <kengo0805ooo@gmail.com>
2020-10-20 19:08:38 +0900

    折り紙の部分の座標を取得できた

--- BWturn.py
+++ BWturn.py
@@ -1,4 +1,7 @@
 import cv2 as cv
+import numpy as np
+import sys
+# 白黒に閾値処理するプログラム
 def main():
     # ファイルを読み込み
     image_file = 'fig/target.png'
@@ -15,6 +18,35 @@ def main():
     retval, dst = cv.threshold(dst, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
     # 結果を保存
     cv.imwrite('fig/result.png', dst)
+
+    # 画像のサイズを取得
+    global height
+    global width
+    height, width, channels = src.shape[:3]
+    print("width: " + str(width))
+    print("height: " + str(height))
+
+
+
+
+# 最初に当たる黒い部分を探す関数
+def search():
+    img = cv.imread('fig/result.png')
+    
+    for i in range(width):
+        for j in range(height):
+            pixelValue = img[i, j]
+            if (pixelValue == [0,0,0]).all():
+                kadoX = i
+                kadoY = j
+            # print('座標[{},{}] = '.format(i,j) + str(pixelValue))
+                print(kadoX,kadoY)
+    # else:
+        # 格納した角の値をprint
+
+    
+
     
 if __name__ == '__main__':
     main()
+    search()
